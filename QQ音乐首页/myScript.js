fab9907d2addd9d80b9c07d01ef9510e9897647c
Dagenjun-kami <58912363+Dagenjun-kami@users.noreply.github.com>
2020-10-02 19:58:37 +0800

    Delete myScript.js

--- QQ音乐首页/myScript.js
+++
@@ -1,9 +0,0 @@
-var topBtn=document.getElementById('topBtn');
-var timer;
-topBtn.onclick=function(){
-    timer=setInterval(function(){
-        var to_top_value=document.body.scrollTop||document.documentElement.scrollTop;
-        console.log(to_top_value)
-        document.body.scrollTop=document.documentElement.scrollTop=to_top_value-10;
-    },10)
-}
